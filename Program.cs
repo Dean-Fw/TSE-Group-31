cd81786f85fa714af7a4754fb141d772a2ae689a
Ben Davies <79200235+bendavies123@users.noreply.github.com>
2022-05-23 18:49:07 +0100

    Code to connect the database to C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace TSEg31Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            string database = "tse_g31_database";
+            string uid = "root";
+            string password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+        }
+    }
+}
