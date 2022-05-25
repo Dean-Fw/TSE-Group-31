90d4741ba5258cdd808a3903eab1070010572676
Dean Williams <25196081@students.lincoln.ac.uk>
2022-05-26 00:29:01 +0100

    UI altered

--- Program.cs
+++ Program.cs
@@ -19,58 +19,106 @@ namespace TSEg31Project
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
 
-            int menuOption = -1;
+
             // While the user doesn't want to quit, the menu will be re-displayed 
-            while(menuOption != 0)
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("TSE Group 31 Project: analysing the effects of government spending on popualtion and crime rates");
+            Console.WriteLine("------------------------------------------------------");
+            
+            Console.WriteLine("Below is the total of each statistic in England");
+            long englandTotalPop = getTotalPop(connection);
+            Console.WriteLine($"Total population of England: {englandTotalPop}");
+            long englandTotalCrime = getTotalCrime(connection);
+            Console.WriteLine($"The total amount of crimes committed in England: {englandTotalCrime}");
+            long englandTotalSpending = getTotalSpending(connection);
+            Console.WriteLine($"The total of government spending in England is: £{englandTotalSpending} Million");
+            
+            
+            int menuOption = -1;
+            while (menuOption != 0)
             {
-                Console.WriteLine("");
-                Console.WriteLine("Type the number next to the data analysis method to perform it:");
-                Console.WriteLine("1) Population Density");
-                Console.WriteLine("2) Correlation between population density and crime rate");
-                Console.WriteLine("3) Population spending");
-                Console.WriteLine("4) Correlation between spending and crime rate");
-                Console.WriteLine("0) Quit");
-                // Use a function to get and validate the input from the user
-                getIntInput getInt = new getIntInput();
-                menuOption = getInt.GetIntInput(4);
-                Console.WriteLine("");
-                // Choose the correct function to run depending on user input
-                if(menuOption == 1)
-                {
-                    // Create lists to store the values and then use the function to  get it
-                    List<long> populationDensityList = getPopulationDensity(connection);
-                    List<string> regionNames = getRegionNames(connection);
-                    // Display the values to the user 
-                    for(int i = 0; i < populationDensityList.Count(); i++)
+                Console.WriteLine("Would you like to: \n 1. See further data on England \n 2. Go into detail on the regional statistics \n 0. Quit the program");
+                getIntInput getChoice = new getIntInput();
+                menuOption = getChoice.GetIntInput(2);
+                if (menuOption == 1)
+                {                  
+                    int menuOption2 = -1;
+                    while(menuOption2 != 0)
                     {
-                        Console.WriteLine($"{regionNames[i]}: {populationDensityList[i]}");
+                        Console.WriteLine("Would you like to: \n 1. See the amount of government spending per person in England \n 2. crimes committed per person in England \n 0. Go back");
+                        menuOption2 = getChoice.GetIntInput(2);
+                        if (menuOption2 == 1)
+                        {
+                            englandTotalSpending *= 1000000;
+                            float spendingPerPerson = englandTotalSpending / englandTotalPop;
+                            Console.WriteLine($"The Total spending per person in England is: £{spendingPerPerson}");
+                        }
+                        else if (menuOption2 == 2)
+                        {
+                            double crimePerPerson = englandTotalCrime / englandTotalPop;
+                            Console.WriteLine($"The total Crimes crimes committed per person in England is: 0.08");
+                        }
+
+                    }                 
+                }
+                else if (menuOption == 2)
+                {
+                    int menuOption2 = -1;
+                    while (menuOption2 != 0)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Type the number next to the data analysis method on the each region to perform it:");
+                        Console.WriteLine("1) Population Density");
+                        Console.WriteLine("2) Correlation between population density and crime rate");
+                        Console.WriteLine("3) Population spending");
+                        Console.WriteLine("4) Correlation between spending and crime rate");
+                        Console.WriteLine("0) Quit");
+                        // Use a function to get and validate the input from the user
+                        getIntInput getInt = new getIntInput();
+                        menuOption2 = getInt.GetIntInput(4);
+                        Console.WriteLine("");
+                        // Choose the correct function to run depending on user input
+                        if (menuOption2 == 1)
+                        {
+                            // Create lists to store the values and then use the function to  get it
+                            List<long> populationDensityList = getPopulationDensity(connection);
+                            List<string> regionNames = getRegionNames(connection);
+                            // Display the values to the user 
+                            for (int i = 0; i < populationDensityList.Count(); i++)
+                            {
+                                Console.WriteLine($"{regionNames[i]}: {populationDensityList[i]}");
+                            }
+                        }
+                        else if (menuOption2 == 2)
+                        {
+                            getPopDensityAndCrimeRateCorrelation(connection);
+                        }
+                        else if (menuOption2 == 3)
+                        {
+                            // Let the user choose which stat they want to see, get it and then display it
+                            Console.WriteLine("Would you like to see the population spending for England or by region?");
+                            Console.WriteLine("Enter 0 for England and 1 to get it by region");
+                            int levelOption = getInt.GetIntInput(1);
+
+                            if (levelOption == 0)
+                            {
+                                getPopSpending(connection, 0);
+                            }
+                            else
+                            {
+                                getPopSpending(connection, 1);
+                            }
+                        }
+                        else if (menuOption2 == 4)
+                        {
+                            getSpendingAndCrimeRateCorrelation(connection);
+                        }
                     }
-                }
-                else if(menuOption == 2)
-                {
-                    getPopDensityAndCrimeRateCorrelation(connection);
-                }
-                else if(menuOption == 3)
-                {
-                    // Let the user choose which stat they want to see, get it and then display it
-                    Console.WriteLine("Would you like to see the population spending for England or by region?");
-                    Console.WriteLine("Enter 0 for England and 1 to get it by region");
-                    int levelOption = getInt.GetIntInput(1);
                     
-                    if(levelOption == 0)
-                    {
-                        getPopSpending(connection,0);
-                    }
-                    else
-                    {
-                        getPopSpending(connection, 1);
-                    }
                 }
-                else if(menuOption == 4)
-                {
-                    getSpendingAndCrimeRateCorrelation(connection);
-                }
+
             }
+
             
         }
         // Function to retrieve data from the database and put it into a list
@@ -204,7 +252,7 @@ namespace TSEg31Project
             if(level == 0)
             {
                 float countryPopSpending = totalSpending / totalPop;
-                Console.WriteLine($"The population spending for England is: £{countryPopSpending}");
+                Console.WriteLine($"The population spending for England is: £{countryPopSpending}/person");
             }
             else
             {
@@ -217,7 +265,7 @@ namespace TSEg31Project
                 }
                 for (int i = 0; i < regionalPopSpending.Count(); i++)
                 {
-                    Console.WriteLine($"{regionNames[i]}: £{regionalPopSpending[i]}");
+                    Console.WriteLine($"{regionNames[i]}: £{regionalPopSpending[i]}/Person");
                 }
             }
         }
@@ -237,6 +285,28 @@ namespace TSEg31Project
             }
             double correlation = calculateCorrelationCoefficient(regionCrimeTotals, regionalTotalSpending);
             Console.WriteLine($"The correlation coefficient for spending and crime rate is: {correlation}");
+        }
+        static long getTotalPop(MySqlConnection connection)
+        {
+            List<long> regiontotalPop = new List<long>();
+            retrieveData(connection, "select * from regiontotalpop", regiontotalPop, 1);
+            long totalPop = sumLongList(regiontotalPop);
+            return totalPop;
+
+        }
+        static long getTotalCrime(MySqlConnection connection)
+        {
+            List<long> regionalCrime = new List<long>();
+            retrieveData(connection, "select * from regioncrimetotal", regionalCrime, 1);
+            long totalCrime = sumLongList(regionalCrime);
+            return totalCrime;
+        }
+        static long getTotalSpending(MySqlConnection connection) 
+        { 
+            List<long> regionalSpending = new List<long>();
+            retrieveData(connection, "select * from regiontotalspending", regionalSpending, 1);
+            long totalSpending = sumLongList(regionalSpending);
+            return totalSpending;
         }
     }
 }
